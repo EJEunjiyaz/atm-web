92c192de4daf59ecae9dbd377e90378ee2281c8f
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2020-10-18 13:58:20 +0700

    Finish XI. (Part two) Hash

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -4,21 +4,46 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.Customer;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerService {
-    private final List<Customer> customers = new ArrayList<>();
+    private List<Customer> customerList;
+
+    @PostConstruct
+    public void postConstruct() {
+        this.customerList = new ArrayList<>();
+    }
 
     public void createCustomer(Customer customer) {
         String hashedPin = hash(customer.getPin());
         customer.setPin(hashedPin);
-        customers.add(customer);
+        customerList.add(customer);
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(customers);
+        return new ArrayList<>(this.customerList);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        return null;
     }
 
     private String hash(String pin) {
