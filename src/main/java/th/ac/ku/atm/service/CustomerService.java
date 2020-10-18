9074344279dbca8419262e3bf7fc4c7716817987
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2020-10-18 13:03:54 +0700

    Finish XI. (Part one) Hash

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,5 +1,6 @@
 package th.ac.ku.atm.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.ku.atm.model.Customer;
 
@@ -11,7 +12,7 @@ public class CustomerService {
     private final List<Customer> customers = new ArrayList<>();
 
     public void createCustomer(Customer customer) {
-        int hashedPin = hash(customer.getPin());
+        String hashedPin = hash(customer.getPin());
         customer.setPin(hashedPin);
         customers.add(customer);
     }
@@ -20,7 +21,8 @@ public class CustomerService {
         return new ArrayList<>(customers);
     }
 
-    private int hash(int value) {
-        return value;
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
     }
 }
